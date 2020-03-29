967d9fa85d59878872e9bf059b8863c5e842655f
takakun <takajcb@gmail.com>
2020-03-29 15:48:47 +0900

    Initial Commit

---
+++ edu_front/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  edu_front
+//
+//  Created by 稲垣　貴士 on 2020/03/29.
+//  Copyright © 2020 Takashi Inagaki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
